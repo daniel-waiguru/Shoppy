4b726fdcccddc5c55c14e39d5d0a6a3c1dea8617
Daniel Waiguru <danielirungu91@gmail.com>
2023-09-29 12:28:02 +0300

    add necessary dependencies

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -38,7 +38,5 @@ dependencies {
     implementation(libs.core.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.test.ext.junit)
     androidTestImplementation(libs.espresso.core)
 }
