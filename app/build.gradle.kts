41a7d8b35b9ff4b1a23e994aac2cf57595dfeea5
Daniel Waiguru <danielirungu91@gmail.com>
2023-09-30 14:54:52 +0300

    add screen transistions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,12 +8,12 @@ plugins {
 
 android {
     namespace = "com.danielwaiguru.shoppy"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.danielwaiguru.shoppy"
         minSdk = 21
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
--- presentation/src/main/java/com/danielwaiguru/shoppy/presentation/navigation/ShoppyNavGraph.kt
+++ presentation/src/main/java/com/danielwaiguru/shoppy/presentation/navigation/ShoppyNavGraph.kt
@@ -1,5 +1,10 @@
 package com.danielwaiguru.shoppy.presentation.navigation
 
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.slideInHorizontally
+import androidx.compose.animation.slideOutVertically
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -14,19 +19,38 @@ fun ShoppyNavGraph(
     modifier: Modifier = Modifier,
     startDestination: String = ProductsScreen.route
 ) {
+    val enterTransitionAnimation = slideInHorizontally (
+        animationSpec = tween(700),
+        initialOffsetX = { it }
+    ) + fadeIn()
+
+    val exitTransitionAnimation = slideOutVertically(
+        animationSpec = tween(700),
+        targetOffsetY = { it }
+    ) + fadeOut(
+        animationSpec = tween(400)
+    )
     NavHost(
         navController = navController,
         startDestination = startDestination,
         modifier = modifier
     ) {
-        composable(ProductsScreen.route) {
+        composable(
+            ProductsScreen.route,
+            enterTransition = { enterTransitionAnimation },
+            exitTransition = { exitTransitionAnimation }
+        ) {
             ProductsRoute(
                 onClick = { productId ->
                     navController.navigate(ProductInfoScreen.createRoute(productId))
                 }
             )
         }
-        composable(ProductInfoScreen.routeWithArgs) {
+        composable(
+            ProductInfoScreen.routeWithArgs,
+            enterTransition = { enterTransitionAnimation },
+            exitTransition = { exitTransitionAnimation }
+        ) {
             ProductInfoRoute(
                 onNavBack = {
                     navController.popBackStack()
